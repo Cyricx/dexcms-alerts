3db7c7d70b8f92506ad4c19c1069780d9e45cf6d
Cyricx <contact@chrisbyram.com>
2016-08-13 12:35:38 -0500

    Updated to use new table directive

--- DexCMS.Alerts.Client/Scripts/Applications/ControlPanel/Modules/Alerts/Alerts/alerts.list.controller.js
+++ DexCMS.Alerts.Client/Scripts/Applications/ControlPanel/Modules/Alerts/Alerts/alerts.list.controller.js
@@ -4,57 +4,45 @@
     app.controller('alertsListCtrl', [
         '$scope',
         'Alerts',
-        'DTOptionsBuilder',
-        'DTColumnBuilder',
-        '$compile',
         '$window',
         '$filter',
-        function ($scope, Alerts, DTOptionsBuilder, DTColumnBuilder, $compile, $window, $filter) {
+        'dexCMSControlPanelSettings',
+        function ($scope, Alerts, $window, $filter, dexcmsSettings) {
             $scope.title = "View Alerts";
 
-            $scope.dtOptions = DTOptionsBuilder.fromFnPromise(function () {
-                return Alerts.getList();
-            }).withBootstrap().withOption('createdRow', createdRow);
-
-            $scope.dtColumns = [
-                DTColumnBuilder.newColumn('alertID').withTitle('ID'),
-                DTColumnBuilder.newColumn('alertText').withTitle('Alert'),
-                DTColumnBuilder.newColumn('startDate').withTitle('Start').renderWith(dateHtml),
-                DTColumnBuilder.newColumn('endDate').withTitle('End').renderWith(dateHtml),
-                DTColumnBuilder.newColumn('displayOrder').withTitle('Order'),
-                DTColumnBuilder.newColumn(null).withTitle('').notSortable().renderWith(actionsHtml)
-            ];
-
-            function dateHtml(data, type, full, meta) {
-                if (data != null) {
-                    return $filter('date')(data, "MM/dd/yyyy h:mm a");
-                } else {
-                    return null;
-                }
-            }
-
-            function createdRow(row, data, dataIndex) {
-                // Recompiling so we can bind Angular directive to the DT
-                $compile(angular.element(row).contents())($scope);
-            }
-
-            function actionsHtml(data, type, full, meta) {
-                var buttons = '<a class="btn btn-warning" ui-sref="alerts/:id({id: +' + data.alertID + '})">' +
-                   '   <i class="fa fa-edit"></i>' +
-                   '</a>';
-                    buttons += ' <button class="btn btn-danger" ng-click="delete(' + data.alertID + ')">' +
-                   '   <i class="fa fa-trash-o"></i>' +
-                   '</button>';
-                return buttons;
-            }
-
-            $scope.delete = function (id) {
-                if (confirm('Are you sure?')) {
-                    Alerts.deleteItem(id).then(function (response) {
-                        $window.location.reload();
-                    });
-                }
+            var _getDateValue = function (value, item) {
+                return $filter('date')(value, "MM/dd/yyyy h:mm a");
             };
+
+            $scope.table = {
+                columns: [
+                    { property: 'alertID', title: 'ID' },
+                    { property: 'alertText', title: 'Alert' },
+                    { property: 'startDate', title: 'Start', dataFunction: _getDateValue },
+                    { property: 'endDate', title: 'End', dataFunction: _getDateValue },
+                    { property: 'displayOrder', title: 'Order' },
+                    {
+                        property: '', title: '', disableSorting: true,
+                        dataTemplate: dexcmsSettings.startingRoute + 'modules/alerts/alerts/_alerts.list.buttons.html'
+                    },
+                ],
+                defaultSort: 'alertID',
+                functions: {
+                    remove: function (id) {
+                        if (confirm('Are you sure?')) {
+                            Alerts.deleteItem(id).then(function (response) {
+                                $window.location.reload();
+                            });
+                        }
+                    }
+                },
+                filePrefix: 'Alerts'
+            };
+
+            Alerts.getList().then(function (data) {
+                $scope.table.promiseData = data;
+            });
+
         }
     ]);
 });
