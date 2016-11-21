9d1e1af6b61b52fe0313fa914a39e3e2e4bfb489
Cyricx <contact@chrisbyram.com>
2016-11-20 22:46:10 -0600

    Added default alert toast view and js

---
+++ DexCMS.Alerts.Mvc/DefaultScripts/Alerts/AlertsToast.js
@@ -0,0 +1,19 @@
+﻿$.ajax({
+    url: '@Url.Content("/alerts/getalerts")',
+    type: 'GET'
+}).done(function (data) {
+    if (data != null && data.length > 0) {
+        var $alertBox = $('#alerts');
+        $alertBox.show();
+        for (var i = 0; i < data.length; i++) {
+            $alertBox.append('<p><i class="fa fa-exclamation-triangle"></i>' + data[i].AlertText + '</p>');
+        }
+    }
+});
+
+var fixHeader = function () {
+    var width = window.innerWidth - 20;
+    $('header').css('width', width);
+};
+
+$(window).on('resize', fixHeader).on('load', fixHeader);
