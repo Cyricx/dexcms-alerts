0cbe07b4d08a454f77271a9182ae6be5db977910
Cyricx <contact@chrisbyram.com>
2016-11-25 18:03:08 -0600

    Added alerts routes to MVC

---
+++ DexCMS.Alerts.Mvc/AlertsMvcRoutes.cs
@@ -0,0 +1,17 @@
+﻿using DexCMS.Core.Infrastructure.Models;
+using System.Web.Routing;
+using System.Web.Mvc;
+
+namespace DexCMS.Alerts.Mvc
+{
+    public static class AlertsMvcRoutes
+    {
+        public static void CreateDefaultRoutes(RouteCollection routes, DexCMSConfiguration config)
+        {
+            routes.MapRoute(
+                name: "Alerts",
+                url: "Alerts/{action}",
+                defaults: new { controller = "Alerts" });
+        }
+    }
+}
