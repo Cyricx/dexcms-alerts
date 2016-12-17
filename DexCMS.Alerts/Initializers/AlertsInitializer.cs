87caefb5792fc886b25fc1c5834e667005ab16f2
Cyricx <contact@chrisbyram.com>
2016-12-17 11:26:55 -0600

    Alerts: Added default alert as an example

---
+++ DexCMS.Alerts/Initializers/AlertsInitializer.cs
@@ -0,0 +1,27 @@
+﻿using DexCMS.Alerts.Contexts;
+using DexCMS.Core.Infrastructure.Globals;
+using System;
+using System.Linq;
+
+namespace DexCMS.Alerts.Initializers
+{
+    public class AlertsInitializer: DexCMSInitializer<IDexCMSAlertsContext>
+    {
+        public AlertsInitializer(IDexCMSAlertsContext context):base(context) { }
+
+        public override void Run()
+        {
+            if (Context.Alerts.Count() == 0)
+            {
+                Context.Alerts.Add(new Models.Alert
+                {
+                    AlertText = "This is a test alert!",
+                    DisplayOrder = 0,
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now.AddDays(1)
+                });
+                Context.SaveChanges();
+            }
+        }
+    }
+}
