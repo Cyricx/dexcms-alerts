a7a008b505c7914b9c114433e61303c2e0e84a3a
Cyricx <contact@chrisbyram.com>
2017-01-28 20:08:40 -0600

    0.4.1-alpha: Added missing mvc initializer

---
+++ DexCMS.Alerts.Mvc/Initializers/AlertsMvcInitializer.cs
@@ -0,0 +1,22 @@
+﻿using DexCMS.Alerts.Contexts;
+using DexCMS.Core.Globals;
+using System;
+using System.Collections.Generic;
+
+namespace DexCMS.Alerts.Mvc.Initializers
+{
+    public class AlertsMvcInitializer : DexCMSLibraryInitializer<IDexCMSAlertsContext>
+    {
+        public AlertsMvcInitializer(IDexCMSAlertsContext context) : base(context)
+        {
+        }
+
+        public override List<Type> Initializers
+        {
+            get
+            {
+                return new List<Type>();
+            }
+        }
+    }
+}
